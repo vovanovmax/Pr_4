5422aea472252703c7ab6a9f150e9175033c9bcb
vovanovmax <vovanovmax@gmail.com>
2026-06-02 23:04:39 +0300

    first commit2

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/logo1.png') {
-        myImage.setAttribute('src', 'images/logo2.jpg');
+    // logo1.webp -> image_0.png, logo2.jpg -> image_1.png
+    if(mySrc === 'assets/img/1.webp') {
+        myImage.setAttribute('src', 'assets/img/2.jpg');
     } else {
-        myImage.setAttribute('src', 'images/logo1.png');
+        myImage.setAttribute('src', 'assets/img/1.webp');
     }
 }
 
@@ -15,12 +16,12 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-    let myName = prompt('Please enter your name.');
+    let myName = prompt('Ласкаво просимо до Opera. Будь ласка, введіть своє ім\'я.');
     if(!myName) {
         setUserName();
     } else {
         localStorage.setItem('name', myName);
-        myHeading.innerHTML = 'JavaScript is cool, ' + myName;
+        myHeading.innerHTML = 'Ласкаво просимо до Opera, ' + myName;
     }
 }
 
@@ -28,7 +29,7 @@ if(!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.innerHTML = 'JavaScript is cool, ' + storedName;
+    myHeading.innerHTML = 'Ласкаво просимо до Opera, ' + storedName;
 }
 
 myButton.onclick = function() {
